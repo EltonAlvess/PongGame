b7ada59eda200fc3b4279076bfd002f942d6c977
eltonalves <eltim.alves@gmail.com>
2016-10-05 15:15:30 +0200

    Finished

--- Assets/Scripts/MoveRacket.cs
+++ Assets/Scripts/MoveRacket.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class MoveRacket : MonoBehaviour {
 	public float speed = 30; 
+	public string axis = "Vertical";
 
 	void FixedUpdate () {
-		float v = Input.GetAxisRaw("Vertical");
+		float v = Input.GetAxisRaw(axis);
 		GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
 	}
 
